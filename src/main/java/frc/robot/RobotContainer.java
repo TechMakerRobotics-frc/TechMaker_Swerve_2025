5763578ecd77850496565f6501b959a704908bc0
23Daniel3 <daneilcipriani321@gmail.com>
2024-12-31 13:21:34 -0300

    Adicionados subsistemas ao RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,15 +25,34 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.drive.*;
+import frc.robot.commands.flywheel.InsideFlywheelCommand;
+import frc.robot.commands.flywheel.OutsideFlywheelCommand;
+import frc.robot.commands.flywheel.StopFlywheelCommand;
+import frc.robot.commands.intake.ExtendIntakeCommand;
+import frc.robot.commands.intake.InsideIntakeCommand;
+import frc.robot.commands.intake.OutsideIntakeCommand;
+import frc.robot.commands.intake.RetractIntakeCommand;
+import frc.robot.commands.intake.StopIntakeCommand;
+import frc.robot.commands.leds.*;
+import frc.robot.commands.lockwheel.AlignBall;
+import frc.robot.commands.lockwheel.InsideLockwheelCommand;
+import frc.robot.commands.lockwheel.OutsideLockwheelCommand;
+import frc.robot.commands.lockwheel.StopLockwheelCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.drive.*;
+import frc.robot.subsystems.flywheel.*;
+import frc.robot.subsystems.intake.*;
+import frc.robot.subsystems.leds.Leds;
+import frc.robot.subsystems.lockwheel.*;
 import frc.robot.subsystems.vision.*;
+import frc.robot.util.RegisNamedCommands;
 import frc.robot.util.zones.ZoneManager;
 import java.io.IOException;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
@@ -44,6 +63,15 @@ public class RobotContainer {
     // Subsystems
     private final Drive drive;
     private final Vision vision;
+    private final Flywheel flywheel;
+    private final Intake intake;
+    private final Lockwheel lockwheel;
+    public final Leds leds;
+
+    // control leds
+    private int currentLedState = 0;
+    private final Command[] ledCommands;
+
     private ZoneManager BlueSpeakerZone;
 
     private SwerveDriveSimulation driveSimulation = null;
@@ -51,8 +79,23 @@ public class RobotContainer {
     // Controller
     private final CommandXboxController controller = new CommandXboxController(0);
 
+    // Operator Controller
+    private final CommandXboxController OperatorController = new CommandXboxController(1);
+
     // Dashboard inputs
     private final LoggedDashboardChooser<Command> autoChooser;
+
+    // tunable flywheel velocity
+    private LoggedNetworkNumber flywheelSpeedInside = new LoggedNetworkNumber("Flywheel Speed Inside", 300.0);
+    private LoggedNetworkNumber flywheelSpeedOutside = new LoggedNetworkNumber("Flywheel Speed Outside", 3000.0);
+
+    // tunable intake velocity
+    private LoggedNetworkNumber intakeSpeedInside = new LoggedNetworkNumber("Intake Speed Inside", 600);
+    private LoggedNetworkNumber intakeSpeedOutside = new LoggedNetworkNumber("Intake Speed Outside", 600);
+
+    // tunable lockwheel velocity
+    private LoggedNetworkNumber lockwheelSpeedInside = new LoggedNetworkNumber("Flywheel Speed Inside", 800);
+    private LoggedNetworkNumber lockwheelSpeedOutside = new LoggedNetworkNumber("Flywheel Speed Outside", 800);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -69,6 +112,11 @@ public class RobotContainer {
                         drive,
                         new VisionIOLimelight(VisionConstants.camera0Name, drive::getRotation),
                         new VisionIOLimelight(VisionConstants.camera1Name, drive::getRotation));
+                flywheel = new Flywheel(new FlywheelIOVictorSPX());
+                intake = new Intake(new IntakeIOSparkMax());
+                lockwheel = new Lockwheel(new LockwheelIOVictorSPX());
+                leds = new Leds();
+                new RegisNamedCommands(flywheel, intake, lockwheel, drive, leds);
 
                 try {
                     BlueSpeakerZone = new ZoneManager(drive, "BlueSpeakerZone");
@@ -97,7 +145,11 @@ public class RobotContainer {
                                 camera0Name, robotToCamera0, driveSimulation::getSimulatedDriveTrainPose),
                         new VisionIOPhotonVisionSim(
                                 camera1Name, robotToCamera1, driveSimulation::getSimulatedDriveTrainPose));
+                flywheel = new Flywheel(new FlywheelIOSim());
+                intake = new Intake(new IntakeIOSim());
+                lockwheel = new Lockwheel(new LockwheelIOSim());
 
+                leds = new Leds();
                 try {
                     BlueSpeakerZone = new ZoneManager(drive, "BlueSpeakerZone");
                 } catch (IOException e) {
@@ -110,7 +162,10 @@ public class RobotContainer {
                 drive = new Drive(
                         new GyroIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {});
                 vision = new Vision(drive, new VisionIO() {}, new VisionIO() {});
-
+                flywheel = new Flywheel(new FlywheelIO() {});
+                intake = new Intake(new IntakeIO() {});
+                lockwheel = new Lockwheel(new LockwheelIO() {});
+                leds = new Leds();
                 try {
                     BlueSpeakerZone = new ZoneManager(drive, "BlueSpeakerZone");
                 } catch (IOException e) {
@@ -136,6 +191,17 @@ public class RobotContainer {
         autoChooser.addOption("Drive to 5 X, 5 Y", new DriveTo(5, 5, 90, 15));
         autoChooser.addOption("Drive to blue speaker zone", new DriveTo(BlueSpeakerZone, 7, 15));
         autoChooser.addOption("AutoChoreo", new ChoreoAuto("FirstAuto", 20));
+
+        ledCommands = new Command[] {
+            new LedRed(leds),
+            new LedGreen(leds),
+            new LedBlue(leds),
+            new LedOff(leds),
+            new LedWhite(leds),
+            new LedYellow(leds),
+            new LedCian(leds),
+            new LedRainbow(leds)
+        };
 
         // Configure the button bindings
         configureButtonBindings();
@@ -168,6 +234,47 @@ public class RobotContainer {
                 : () -> drive.setPose(new Pose2d(drive.getPose().getTranslation(), new Rotation2d())); // zero gyro
 
         controller.povRight().onTrue(Commands.runOnce(resetGyro, drive).ignoringDisable(true));
+
+        // Operator commands
+
+        // flywheel
+        OperatorController.y()
+                .onTrue(new OutsideFlywheelCommand(flywheel, flywheelSpeedOutside.get()))
+                .onFalse(new StopFlywheelCommand(flywheel));
+        OperatorController.a()
+                .onTrue(new InsideFlywheelCommand(flywheel, flywheelSpeedInside.get()))
+                .onFalse(new StopFlywheelCommand(flywheel));
+
+        // intake
+        OperatorController.povUp()
+                .onTrue(new InsideIntakeCommand(intake, intakeSpeedInside.get()))
+                .onFalse(new StopIntakeCommand(intake));
+        OperatorController.povDown()
+                .onTrue(new OutsideIntakeCommand(intake, intakeSpeedOutside.get()))
+                .onFalse(new StopIntakeCommand(intake));
+
+        OperatorController.povLeft().onTrue(new ExtendIntakeCommand(intake));
+        OperatorController.povRight().onTrue(new RetractIntakeCommand(intake));
+
+        OperatorController.leftBumper().onTrue(new AlignBall(lockwheel));
+
+        // lockwheel
+        OperatorController.x()
+                .onTrue(new OutsideLockwheelCommand(lockwheel, lockwheelSpeedInside.get()))
+                .onFalse(new StopLockwheelCommand(lockwheel));
+        OperatorController.b()
+                .onTrue(new InsideLockwheelCommand(lockwheel, lockwheelSpeedOutside.get()))
+                .onFalse(new StopLockwheelCommand(lockwheel));
+
+        // leds
+        OperatorController.leftStick().onTrue(Commands.runOnce(() -> {
+            // Alterna o comando atual para o próximo na lista
+            currentLedState = (currentLedState + 1) % ledCommands.length;
+            Command nextCommand = ledCommands[currentLedState];
+
+            // Executa o próximo comando
+            nextCommand.schedule();
+        }));
     }
 
     /**
---
+++ src/main/java/frc/robot/util/RegisNamedCommands.java
@@ -0,0 +1,84 @@
+package frc.robot.util;
+
+import com.pathplanner.lib.auto.NamedCommands;
+// import frc.robot.commands.drive.AlignToTag;
+import frc.robot.commands.flywheel.*;
+import frc.robot.commands.intake.*;
+import frc.robot.commands.leds.*;
+import frc.robot.commands.lockwheel.*;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.flywheel.Flywheel;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.leds.Leds;
+import frc.robot.subsystems.lockwheel.Lockwheel;
+// import frc.robot.vision.VisionPose;
+
+public class RegisNamedCommands {
+
+    private Flywheel flywheel;
+    private Intake intake;
+    private Lockwheel lockwheel;
+    // private VisionPose visionPose;
+    // private Drive drive;
+    private Leds leds;
+
+    /** Register commands in the pathplanner. */
+    public RegisNamedCommands(
+            Flywheel flywheel,
+            Intake intake,
+            Lockwheel lockwheel,
+            // VisionPose visionPose,
+            Drive drive,
+            Leds leds) {
+        this.flywheel = flywheel;
+        this.intake = intake;
+        this.lockwheel = lockwheel;
+        // this.visionPose = visionPose;
+        // this.drive = drive;
+        this.leds = leds;
+
+        RegisterFlywheel();
+        RegisterIntake();
+        RegisterLockwheel();
+        // RegisterAlign();
+        RegisterLeds();
+    }
+
+    private void RegisterFlywheel() {
+        NamedCommands.registerCommand("Outside Flywheel", new OutsideFlywheelCommand(flywheel, 1000));
+        NamedCommands.registerCommand("Inside Flywheel", new InsideFlywheelCommand(flywheel, 1000));
+        NamedCommands.registerCommand("Stop Flywheels", new StopFlywheelCommand(flywheel));
+        /*NamedCommands.registerCommand(
+        "Flywheel Distance", new FlywheelDistanceCommand(flywheel, visionPose));*/
+    }
+
+    private void RegisterIntake() {
+        NamedCommands.registerCommand("Inside Intake", new InsideIntakeCommand(intake, 250));
+        NamedCommands.registerCommand("Outside Intake", new OutsideIntakeCommand(intake, 300));
+        NamedCommands.registerCommand("Stop Intake", new StopIntakeCommand(intake));
+        NamedCommands.registerCommand("Extend Intake", new ExtendIntakeCommand(intake));
+        NamedCommands.registerCommand("Retract Intake", new RetractIntakeCommand(intake));
+    }
+
+    private void RegisterLockwheel() {
+        NamedCommands.registerCommand("Inside Lockwheel", new InsideLockwheelCommand(lockwheel, 1000.00));
+        NamedCommands.registerCommand("Outside Lockwheel", new OutsideLockwheelCommand(lockwheel, 1000.00));
+        NamedCommands.registerCommand("Stop Lockwheel", new StopLockwheelCommand(lockwheel));
+        NamedCommands.registerCommand("Align Ball", new AlignBall(lockwheel));
+    }
+
+    /*private void RegisterAlign() {
+      NamedCommands.registerCommand("Align", new AlignToTag(drive, visionPose, 20000));
+    }*/
+
+    private void RegisterLeds() {
+        NamedCommands.registerCommand("Led Red", new LedRed(leds));
+        NamedCommands.registerCommand("Led Green", new LedGreen(leds));
+        NamedCommands.registerCommand("Led Blue", new LedBlue(leds));
+        NamedCommands.registerCommand("Led Yellow", new LedYellow(leds));
+        NamedCommands.registerCommand("Led Cian", new LedCian(leds));
+        NamedCommands.registerCommand("Led White", new LedWhite(leds));
+        NamedCommands.registerCommand("Led Rainbow", new LedRainbow(leds));
+        NamedCommands.registerCommand("Led Off", new LedOff(leds));
+    }
+}
