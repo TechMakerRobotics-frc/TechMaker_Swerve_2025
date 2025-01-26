9e30ea1af260386426bc17a4367d474c743ee7ec
23Daniel3 <daneilcipriani321@gmail.com>
2025-01-26 16:06:36 -0300

    Removidos dados inutilizados de AllianceFlip

--- src/main/java/frc/robot/util/AllianceFlipUtil.java
+++ src/main/java/frc/robot/util/AllianceFlipUtil.java
@@ -1,19 +1,10 @@
-// Copyright (c) 2025 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
-package org.littletonrobotics.frc2025.util;
+package frc.robot.util;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
-import org.littletonrobotics.vehicletrajectoryservice.VehicleTrajectoryServiceOuterClass.ModuleForce;
-import org.littletonrobotics.vehicletrajectoryservice.VehicleTrajectoryServiceOuterClass.VehicleState;
 
 public class AllianceFlipUtil {
   public static double fieldWidth = Units.feetToMeters(26.0) + Units.inchesToMeters(5.0);
@@ -39,28 +30,6 @@ public class AllianceFlipUtil {
     return shouldFlip()
         ? new Pose2d(apply(pose.getTranslation()), apply(pose.getRotation()))
         : pose;
-  }
-
-  public static VehicleState apply(VehicleState state) {
-    return shouldFlip()
-        ? VehicleState.newBuilder()
-            .setX(applyX(state.getX()))
-            .setY(applyY(state.getY()))
-            .setTheta(apply(Rotation2d.fromRadians(state.getTheta())).getRadians())
-            .setVx(-state.getVx())
-            .setVy(-state.getVy())
-            .setOmega(state.getOmega())
-            .addAllModuleForces(
-                state.getModuleForcesList().stream()
-                    .map(
-                        forces ->
-                            ModuleForce.newBuilder()
-                                .setFx(-forces.getFx())
-                                .setFy(-forces.getFy())
-                                .build())
-                    .toList())
-            .build()
-        : state;
   }
 
   public static boolean shouldFlip() {
