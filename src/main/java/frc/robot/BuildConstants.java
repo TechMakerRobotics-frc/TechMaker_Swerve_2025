56ac74a28fbfeb11caa65163e3bf64d1a98119b9
Johnny Dreher <johnny.dreher@teksea.net>
2025-01-31 16:15:31 -0300

    Ajustadas as variaveis geradas

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TechMaker_Swerve_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 74;
-  public static final String GIT_SHA = "5ae0dfb9d8d9b837fc73164df6a655f4cde9d436";
-  public static final String GIT_DATE = "2025-01-31 17:07:29 GMT";
+  public static final int GIT_REVISION = 75;
+  public static final String GIT_SHA = "df31306ff778ecdecd58fd6846d7e3cb3cb9dc5e";
+  public static final String GIT_DATE = "2025-01-31 16:05:44 BRT";
   public static final String GIT_BRANCH = "Hardware_funcional_BotRelative";
-  public static final String BUILD_DATE = "2025-01-31 19:04:08 GMT";
-  public static final long BUILD_UNIX_TIME = 1738350248003L;
+  public static final String BUILD_DATE = "2025-01-31 16:12:23 BRT";
+  public static final long BUILD_UNIX_TIME = 1738350743503L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
