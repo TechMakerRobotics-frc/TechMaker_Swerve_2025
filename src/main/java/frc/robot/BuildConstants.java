df31306ff778ecdecd58fd6846d7e3cb3cb9dc5e
Johnny Dreher <johnny.dreher@teksea.net>
2025-01-31 16:05:44 -0300

    Atualizadas funções para constantes do github e deploy automatico em eventos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TechMaker_Swerve_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "be14a677022245edef71da3219aab26b20692a10";
-  public static final String GIT_DATE = "2025-01-02 13:06:32 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-04 19:19:30 EST";
-  public static final long BUILD_UNIX_TIME = 1736036370445L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "5ae0dfb9d8d9b837fc73164df6a655f4cde9d436";
+  public static final String GIT_DATE = "2025-01-31 17:07:29 GMT";
+  public static final String GIT_BRANCH = "Hardware_funcional_BotRelative";
+  public static final String BUILD_DATE = "2025-01-31 19:04:08 GMT";
+  public static final long BUILD_UNIX_TIME = 1738350248003L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
