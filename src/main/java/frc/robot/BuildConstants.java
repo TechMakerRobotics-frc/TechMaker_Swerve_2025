0eaffa17a44dc586f33584e88a576de59b16ee2d
TechMaker <122989919+techmaker1234@users.noreply.github.com>
2025-02-01 17:26:14 -0300

    Adicionado variaveis yagsl

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TechMaker_Swerve_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 76;
-  public static final String GIT_SHA = "56ac74a28fbfeb11caa65163e3bf64d1a98119b9";
-  public static final String GIT_DATE = "2025-01-31 16:15:31 BRT";
-  public static final String GIT_BRANCH = "Hardware_funcional_BotRelative";
-  public static final String BUILD_DATE = "2025-01-31 17:59:51 BRT";
-  public static final long BUILD_UNIX_TIME = 1738357191603L;
+  public static final int GIT_REVISION = 78;
+  public static final String GIT_SHA = "b603b0fe22052b404b7e2f9cb81b2bac3cac5be6";
+  public static final String GIT_DATE = "2025-01-31 18:17:07 BRT";
+  public static final String GIT_BRANCH = "Teste_Odometria";
+  public static final String BUILD_DATE = "2025-02-01 11:16:22 BRT";
+  public static final long BUILD_UNIX_TIME = 1738419382351L;
   public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
