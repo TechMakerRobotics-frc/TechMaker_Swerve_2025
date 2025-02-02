8a8e9d537c863af72d78c79d79a2842dd3ee68d3
23Daniel3 <daneilcipriani321@gmail.com>
2025-02-02 01:59:43 -0300

    Adicionando contantes de simulação à DriveConstants

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,11 +1,37 @@
 package frc.robot.subsystems.drive;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.List;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.ParentConfiguration;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.ClosedLoopOutputType;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerFeedbackType;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.SteerMotorArrangement;
+import com.ctre.phoenix6.swerve.SwerveModuleConstantsFactory;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.util.Circle2d;
 
 public class DriveConstants {
@@ -39,10 +65,20 @@ public class DriveConstants {
         }
     }
 
-    public static final class ModuleConstants {
+    public static final double MAX_SPEED_MTS_SEC = 14.5;
+
     // Gear ratios for SDS MK4i L3, adjust as necessary
     public static final double DRIVE_GEAR_RATIO = (50.0 / 14.0) * (16.0 / 28.0) * (45.0 / 15.0);
     public static final double TURN_GEAR_RATIO = 150.0 / 7.0;
+
+    public static final Slot0Configs STEER_GAINS = new Slot0Configs()
+            .withKP(100)
+            .withKI(0)
+            .withKD(2.0)
+            .withKS(0.2)
+            .withKV(1.59)
+            .withKA(0)
+            .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
 
     public static final Slot0Configs DRIVE_GAINS =
         new Slot0Configs().withKP(0.03).withKI(0).withKD(0).withKS(0).withKV(0.124);
@@ -53,24 +89,40 @@ public class DriveConstants {
     public static final int SPARK_FL = 2;
     public static final int CANCODER_FL = 3;
     public static final double ENCODER_OFFSET_FL = (-0.408203);
+    public static final Angle ENCODER_OFFSET_FL_ANGLE = Rotations.of(ENCODER_OFFSET_FL);
+    public static final boolean ENCODER_INVERTED_FL = false;
+    public static final Distance X_DISTANCE_FL = Inches.of(10.5);
+    public static final Distance Y_DISTANCE_FL = Inches.of(10.5);
 
     // Front Right
     public static final int TALON_FR = 4;
     public static final int SPARK_FR = 5;
     public static final int CANCODER_FR = 6;
     public static final double ENCODER_OFFSET_FR = (0.325195);
-
+    public static final Angle ENCODER_OFFSET_FR_ANGLE = Rotations.of(ENCODER_OFFSET_FR);
+    public static final boolean ENCODER_INVERTED_FR = false;
+    public static final Distance X_DISTANCE_FR = Inches.of(10.5);
+    public static final Distance Y_DISTANCE_FR = Inches.of(-10.5);
+    
     // Back Left
     public static final int TALON_BL = 7;
     public static final int SPARK_BL = 8;
     public static final int CANCODER_BL = 9;
     public static final double ENCODER_OFFSET_BL = (0.389404);
-
+    public static final Angle ENCODER_OFFSET_BL_ANGLE = Rotations.of(ENCODER_OFFSET_BL);
+    public static final boolean ENCODER_INVERTED_BL = false;
+    public static final Distance X_DISTANCE_BL = Inches.of(-10.5);
+    public static final Distance Y_DISTANCE_BL = Inches.of(10.5);
+    
     // Back Right
     public static final int TALON_BR = 10;
     public static final int SPARK_BR = 11;
     public static final int CANCODER_BR = 12;
     public static final double ENCODER_OFFSET_BR = (0.374023);
+    public static final Angle ENCODER_OFFSET_BR_ANGLE = Rotations.of(ENCODER_OFFSET_BR);
+    public static final boolean ENCODER_INVERTED_BR = false;
+    public static final Distance X_DISTANCE_BR = Inches.of(-10.5);
+    public static final Distance Y_DISTANCE_BR = Inches.of(-10.5);
 
     public static final String CANBUS = "CANivore";
 
@@ -110,5 +162,95 @@ public class DriveConstants {
     public static final double turnSimD = 0.0;
     public static final double turnPIDMinInput = 0; // Radians
     public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
-  }
+
+    public static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+    public static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
+            .withCurrentLimits(new CurrentLimitsConfigs()
+                    // Swerve azimuth does not require much torque output, so we can set a
+                    // relatively
+                    // low
+                    // stator current limit to help avoid brownouts without impacting performance.
+                    .withStatorCurrentLimit(Amps.of(60))
+                    .withStatorCurrentLimitEnable(true));
+    public static final CANcoderConfiguration cancoderInitialConfigs = new CANcoderConfiguration();
+
+    public static final boolean kInvertLeftSide = false;
+    public static final boolean kInvertRightSide = true;
+
+    public static final SwerveModuleConstantsFactory<
+          ParentConfiguration, ParentConfiguration, ParentConfiguration>
+      ConstantCreatorSIM =
+          new SwerveModuleConstantsFactory<>()
+              .withDriveMotorGearRatio(DRIVE_GEAR_RATIO)
+              .withSteerMotorGearRatio(TURN_GEAR_RATIO)
+              .withCouplingGearRatio(kCoupleRatio)
+              .withWheelRadius(kWheelRadius)
+              .withSteerMotorGains(STEER_GAINS)
+              .withDriveMotorGains(DRIVE_GAINS)
+              .withSteerMotorClosedLoopOutput(kSteerClosedLoopOutput)
+              .withDriveMotorClosedLoopOutput(kDriveClosedLoopOutput)
+              .withSlipCurrent(kSlipCurrent)
+              .withSpeedAt12Volts(MAX_SPEED_MTS_SEC)
+              .withFeedbackSource(kSteerFeedbackType)
+              .withDriveMotorInitialConfigs(driveInitialConfigs)
+              .withSteerMotorInitialConfigs(steerInitialConfigs)
+              .withEncoderInitialConfigs(cancoderInitialConfigs)
+              .withSteerInertia(kSteerInertia)
+              .withDriveInertia(kDriveInertia)
+              .withSteerFrictionVoltage(kSteerFrictionVoltage)
+              .withDriveFrictionVoltage(kDriveFrictionVoltage);
+
+  public static final SwerveModuleConstants<
+          ParentConfiguration, ParentConfiguration, ParentConfiguration>
+      FrontLeftSIM =
+          ConstantCreatorSIM.createModuleConstants(
+              SPARK_FL,
+              TALON_FL,
+              CANCODER_FL,
+              ENCODER_OFFSET_FL_ANGLE,
+              X_DISTANCE_FL,
+              Y_DISTANCE_FL,
+              kInvertLeftSide,
+              TURN_MOTOR_INVERTED,
+              ENCODER_INVERTED_FL
+              );
+  public static final SwerveModuleConstants<
+          ParentConfiguration, ParentConfiguration, ParentConfiguration>
+      FrontRightSIM =
+          ConstantCreatorSIM.createModuleConstants(
+              TALON_FR,
+              SPARK_FR,
+              CANCODER_FR,
+              ENCODER_OFFSET_FR_ANGLE,
+              X_DISTANCE_FR,
+              Y_DISTANCE_FR,
+              kInvertRightSide,
+              TURN_MOTOR_INVERTED,
+              ENCODER_INVERTED_FL);
+  public static final SwerveModuleConstants<
+          ParentConfiguration, ParentConfiguration, ParentConfiguration>
+      BackLeftSIM =
+          ConstantCreatorSIM.createModuleConstants(
+              kBackLeftSteerMotorId,
+              kBackLeftDriveMotorId,
+              kBackLeftEncoderId,
+              kBackLeftEncoderOffset,
+              kBackLeftXPos,
+              kBackLeftYPos,
+              kInvertLeftSide,
+              kBackLeftSteerMotorInverted,
+              kBackLeftCANcoderInverted);
+  public static final SwerveModuleConstants<
+          ParentConfiguration, ParentConfiguration, ParentConfiguration>
+      BackRightSIM =
+          ConstantCreatorSIM.createModuleConstants(
+              kBackRightSteerMotorId,
+              kBackRightDriveMotorId,
+              kBackRightEncoderId,
+              kBackRightEncoderOffset,
+              kBackRightXPos,
+              kBackRightYPos,
+              kInvertRightSide,
+              kBackRightSteerMotorInverted,
+              kBackRightCANcoderInverted);
 }
